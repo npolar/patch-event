131ffc43ef3abd69a59259dcd5a4f59d2411d3f4
cnrdh <conrad@npolar.no>
2019-10-30 09:10:44 +0100

    Renaming demos

--- demo/mini.js
+++ demo/mini.js
@@ -1,17 +1,55 @@
 import { register } from "../src/host.js";
 import { patch } from "../src/patch.js";
 const { stringify } = JSON;
+const { log } = console;
 
 const handlePatchEvent = event => {
-  console.log("patch-op", event.detail);
+  console.log("patch", event.detail);
   const { op, path, value } = event.detail;
-  data = patch(data, { op, path, value });
-  output.textContent = stringify(data);
+  set({ op, path, value });
+  update();
 };
 
-let data = { foo: null };
-const form = document.querySelector("form");
-const output = document.querySelector("output");
-output.textContent = stringify(data);
+const _states = [{ foo: "bar" }];
+const _patches = [];
+let cursor = 0;
 
+const get = () => _states[cursor];
+
+const set = ({ op, path, value }, prev = get()) => {
+  const next = patch(get(), { op, path, value });
+  _states[++cursor] = next;
+};
+
+const _undo = e => {
+  e.preventDefault();
+  cursor = cursor > 1 ? --cursor : 0;
+  update();
+};
+
+const _redo = e => {
+  e.preventDefault();
+  cursor = cursor < _states.length - 1 ? ++cursor : _states.length - 1;
+  update();
+};
+
+const form = document.querySelector("form");
 register(form, handlePatchEvent);
+
+const undo = document.querySelector("#undo");
+undo.addEventListener("click", _undo);
+
+const redo = document.querySelector("#redo");
+redo.addEventListener("click", _redo);
+
+const update = (state = get()) => {
+  const output = document.querySelector("output");
+  const foo = document.querySelector("#foo");
+  const bar = document.querySelector("#bar");
+
+  output.textContent = stringify(state);
+  foo.value = state.foo || "";
+  bar.value = state.bar || "";
+};
+
+update();
