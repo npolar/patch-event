7db6ddd3788ae05f03e575d71fc3a46eda64867a
cnrdh <conrad@npolar.no>
2020-01-07 15:26:11 +0100

    Refactored and purified

--- src/patch.js
+++ src/patch.js
@@ -1,14 +1,53 @@
 import { deepCopy } from "./deep-copy.js";
-import { apply as _mutatingApplyPatch } from "./rfc6902/patch.js";
+import { apply as _mutatingApplyPatch, MissingError } from "./rfc6902/patch.js";
+import { isDestructive } from "./rfc6902/diff.js";
 
 const { fromEntries } = Object;
 const { stringify, parse } = JSON;
 const _jsonDeepCopy = o => parse(stringify(o));
+const _realPath = path =>
+  path && path.length && path.length > 1 && path.startsWith("/");
+const _copier = fromEntries ? deepCopy : _jsonDeepCopy;
 
-// Apply a single non-mutating patch operation
-export const patch = (object, operation) => {
-  const copy = fromEntries ? deepCopy(object) : _jsonDeepCopy(object);
-  const { op, path, value } = operation;
-  _mutatingApplyPatch(copy, { op, path, value });
+export const ops = ["add", "remove", "replace", "move", "copy", "test"];
+
+// Apply a single non-mutating patch operation on a deep copy of the provided object
+export const patch = (
+  object,
+  { op, path, value, from, ...extra },
+  {
+    copier = _copier,
+    isAcceptablePath = _realPath,
+    isValidOp = (op, arr = ops) => arr.includes(op)
+  } = {}
+) => {
+  extra;
+  //console.log({ extra });
+  if (!op) {
+    throw new MissingError("/op");
+  }
+  if (!path) {
+    throw new MissingError("/path");
+  }
+  if (["move", "copy"].includes(op)) {
+    if (!from) {
+      throw new MissingError("/from");
+    }
+  }
+  if (!isValidOp(op)) {
+    throw `Invalid patch operation: "${op}"`;
+  }
+  // The condition below is to protect the innocent, as "" and "/" are valid paths ([json-pointer](http://jsonpatch.com/#json-pointer))s:
+  // "To point to the root of the document use an empty string for the pointer.
+  // The pointer / doesn’t point to the root, it points to a key of "" on the root (which is totally valid in JSON).""
+  if (isDestructive(op) && !isAcceptablePath(path)) {
+    throw "Path not acceptable for destructive operation";
+  }
+
+  const copy = copier(object);
+  const error = _mutatingApplyPatch(copy, { op, path, value, from });
+  if (error) {
+    throw error;
+  }
   return copy;
 };
