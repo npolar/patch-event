ea6a349852fcef29bfbbf79d14899a857a5399fe
cnrdh <conrad@npolar.no>
2020-01-07 15:32:57 +0100

    Added "from"; moving extra into own key in the operation object: fixed not-null

--- src/operation.js
+++ src/operation.js
@@ -1,26 +1,39 @@
 import { originator } from "./originator.js";
-import { extractOp, extractPath, extractValue } from "./extract.js";
+import {
+  extractOp,
+  extractPath,
+  extractValue,
+  extractFrom
+} from "./extract.js";
+import { hasSelfOrAncestorAttribute } from "./host.js";
 
 export const operation = event => {
-  const from = originator(event);
-  const op = extractOp(from);
-  const { type, validity } = from;
+  const host = originator(event);
+  const op = extractOp(host);
+  const { type, validity } = host;
   // type: HTML [input type](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#Form_%3Cinput%3E_types) (text, number, email, password, ...)
   // validity:  HTML [validity state(https://developer.mozilla.org/en-US/docs/Web/API/ValidityState)
-  const nullable = !from.hasAttribute("not-null"); // ie. nullable is default
-  const path = extractPath(from); // JSON Pointer
-  const value = extractValue(from, type, { nullable });
+  const notnull = hasSelfOrAncestorAttribute(host, "not-null"); // ie. nullable is default
+  const nullable = !notnull;
 
+  const path = extractPath(host); // path is a JSON Pointer
+  const value = extractValue(host, { type, nullable }); // type is html host's type
+  const from = extractFrom(host);
   let valid = validity && "valid" in validity ? validity.valid : null;
   if (false === valid && null === value && nullable) {
     valid = true;
   }
-  return {
+
+  const operation = {
     op,
     path,
     value,
-    type,
-    valid,
-    nullable
+    from,
+    extra: {
+      type,
+      valid,
+      nullable
+    }
   };
+  return operation;
 };
